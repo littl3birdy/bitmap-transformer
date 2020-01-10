78134260bffaa1d3cb9cbee6a0c6582643f2b914
littl3birdy <ashlynmmorin@gmail.com>
2020-01-10 13:35:21 -0800

    worked on greyscale transform method

--- src/main/java/bitmap/transformer/Bitmap.java
+++ src/main/java/bitmap/transformer/Bitmap.java
@@ -1,6 +1,7 @@
 package bitmap.transformer;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -36,7 +37,18 @@ public class Bitmap {
         try {
             BufferedImage bitOG = ImageIO.read(this.bitFile);
 //            BufferedImage bitMono = bitOG.setRGB(0, 0, 10);
+            //We used this resource for this code block: https://www.tutorialspoint.com/java_dip/grayscale_conversion.htm
+            for(int i=0; i <bitOG.getHeight(); i++) {
+                for(int j=0; j<bitOG.getWidth(); j++){
+                    Color bitColor = new Color(bitOG.getRGB(i,j));
+                    int r = (int)(bitColor.getRed()*0.299);
+                    int g = (int)(bitColor.getGreen()*0.587);
+                    int b = (int)(bitColor.getBlue()*0.114);
+                    Color newBitColor = new Color(r+b+g,r+b+g,r+b+g);
 
+                    bitOG.setRGB(j,i,newBitColor.getRGB());
+                }
+            }
             this.bitOutputFile(bitOG);
 
         } catch (IOException e) {
